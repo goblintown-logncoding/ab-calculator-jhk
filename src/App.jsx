4b926a7085cd2ae2c6a5ed34a0ee47aec4e2cadf
Juha Kim <student@Emmas-MacBook.local>
2024-08-01 21:20:26 +0900

    feat: add Header, Content, Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 
 import './App.css'
+import Header from'./Header';
+import Footer from './Footer';
+import Content from './Content';
 
-function App() {
-
+const App = () => {
   return (
-  <>
-  </>)
+    <>
+      <Header />
+      <Footer />
+      <Content />
+
+    </>
+
+  )
 }
+export default App;
